27c221d1960f067c9709bee9d4af5cf8159330b8
Arne Neumann <github@arne.cl>
2014-07-03 23:47:03 +0200

    catch exceptions during collection

--- collect.py
+++ collect.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python
 
 import os
+import sys
 import sh
 
 from config import SRC, PATHS
 
 for path in PATHS:
 	for filename in os.listdir(path):
-		if filename.endswith('.pdf') and not os.path.exists(os.path.join(SRC, filename)):
-			sh.ln('-s', os.path.join(path, filename), os.path.join(SRC, filename))
+		try:
+			if filename.endswith('.pdf') and not os.path.exists(os.path.join(SRC, filename)):
+				sh.ln('-s', os.path.join(path, filename), os.path.join(SRC, filename))
+		except Exception as e:
+			sys.stderr.write(u"Can't create symlink to file '{}'.\n{}".format(filename,e))
+			pass
