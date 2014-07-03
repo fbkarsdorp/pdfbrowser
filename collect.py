5c8240c4eab488605eead2f21f4c76c3b6d28e85
Arne Neumann <github@arne.cl>
2014-07-04 00:16:53 +0200

    collect files from subdirectories as well

--- collect.py
+++ collect.py
@@ -7,10 +7,11 @@ import sh
 from config import SRC, PATHS
 
 for path in PATHS:
-	for filename in os.listdir(path):
-		try:
-			if filename.endswith('.pdf') and not os.path.exists(os.path.join(SRC, filename)):
-				sh.ln('-s', os.path.join(path, filename), os.path.join(SRC, filename))
-		except Exception as e:
-			sys.stderr.write(u"Can't create symlink to file '{}'.\n{}".format(filename,e))
-			pass
+	for folder, subfolders, filenames in os.walk(path):
+		for filename in filenames:
+			try:
+				if filename.endswith('.pdf') and not os.path.exists(os.path.join(SRC, filename)):
+					sh.ln('-s', os.path.join(path, filename), os.path.join(SRC, filename))
+			except Exception as e:
+				sys.stderr.write(u"Can't create symlink to file '{}'.\n{}".format(filename.decode('utf8'),e))
+				pass
