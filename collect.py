b1c8993a7a50e84aad7b99b6c69945e4171cc867
Arne Neumann <github@arne.cl>
2014-07-03 23:27:35 +0200

    create a symlink to PDFs instead of copying them

--- collect.py
+++ collect.py
@@ -8,4 +8,4 @@ from config import SRC, PATHS
 for path in PATHS:
 	for filename in os.listdir(path):
 		if filename.endswith('.pdf') and not os.path.exists(os.path.join(SRC, filename)):
-			sh.mv(os.path.join(path, filename), os.path.join(SRC, filename))
+			sh.ln('-s', os.path.join(path, filename), os.path.join(SRC, filename))
