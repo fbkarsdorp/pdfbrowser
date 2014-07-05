2640464daec18d048d3921e0293265897c707fc2
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-07-05 12:39:39 +0200

    add PDFTOTEXT_PATH and automatically expand user directory

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
+
 ROOT = '/Users/folgert/pdfviewer/'
 DATA = os.path.join(ROOT, 'data')
 SRC = os.path.join(ROOT, 'static/sources')
+PDFTOTEXT_PATH = '/usr/local/bin/pdftotext'
 
-PATHS = ['/Users/folgert/Downloads']
+PATHS = [os.path.join(os.path.expanduser("~"), 'Downloads')]
